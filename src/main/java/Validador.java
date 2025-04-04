978fc82493f6aa2f2d10d4d7940068a1dcc98f73
anaijg <ana.jimenezgomez@educa.madrid.org>
2025-04-04 17:46:33 +0200

    terminado

--- src/main/java/Validador.java
+++ src/main/java/Validador.java
@@ -29,22 +29,32 @@ public class Validador {
 
     //Ejercicio 2. Validar nombre
     //Escribe un método que reciba un String y verifique si contiene solo letras (incluidos acentos y ñ).
-
+    public static boolean validarNombre(String nombre) {
+        return nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]+");
+    }
 
 
     //Ejercicio 3. Validar código postal
     //Escribe un método que compruebe si un String contiene exactamente cinco cifras.
-
+    public static boolean validarCP(String codigo) {
+        return codigo.matches("\\d{5}");
+    }
 
     //Ejercicio 4. Validar correo electrónico
     //Escribe un método que determine si una cadena tiene el formato básico de un email.
-
+    public static boolean validarEmail(String email) {
+        return email.matches("\\w+@\\w+\\.\\w{2,5}");
+    }
 
     //Ejercicio 5. Validar fecha formato dd/mm/aaaa
     //Escribe un método que reciba una fecha en forma de String y compruebe si está en formato dd/mm/aaaa.
-
+    public static boolean validarFecha(String fecha) {
+        return fecha.matches("\\d{2}/\\d{2}/\\d{4}");
+    }
 
     //Ejercicio 6. Validar contraseña segura
     //Escribe un método que verifique si una contraseña contiene al menos una letra minúscula, una mayúscula, un número y tiene al menos 6 caracteres.
-
+    public static boolean validarPassword(String password) {
+        return password.matches("(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}");
+    }
 }
--- src/test/java/EjerciciosRegExpTest.java
+++ src/test/java/EjerciciosRegExpTest.java
@@ -6,7 +6,34 @@ class EjerciciosRegExpTest {
     void testTelefono() {
         assertTrue(Validador.validarTelefono("612345678"));
         assertFalse(Validador.validarTelefono("61234"));
+        assertFalse(Validador.validarTelefono("6123A5678"));
+        assertFalse(Validador.validarTelefono("+34 6123A5678"));
+        assertFalse(Validador.validarTelefono("+34 612 345 678"));
     }
 
+    @Test
+    void testNombre() {
+        assertTrue(Validador.validarNombre("Lucía"));
+        assertFalse(Validador.validarNombre("Lucía2"));
+        assertFalse(Validador.validarNombre(""));
+        assertTrue(Validador.validarNombre("Aquilino"));
+    }
 
+    @Test
+    void testEmail() {
+        assertTrue(Validador.validarEmail("persona@email.com"));
+        assertFalse(Validador.validarEmail("persona@correo"));
+    }
+
+    @Test
+    void testFecha() {
+        assertTrue(Validador.validarFecha("23/04/2025"));
+        assertFalse(Validador.validarFecha("23-04-2025"));
+    }
+
+    @Test
+    void testPassword() {
+        assertTrue(Validador.validarPassword("Clave123"));
+        assertFalse(Validador.validarPassword("clave"));
+    }
 }
